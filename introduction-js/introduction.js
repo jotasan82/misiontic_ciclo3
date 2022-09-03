9a64ebdb354e57ddd0103f16bb61aee01cae439b
jotasan82 <ingeniero.juliocesar82>
2022-09-02 23:37:43 -0500

    introduction-js: introduction to array and objects

---
+++ introduction-js/introduction.js
@@ -0,0 +1,51 @@
+/**
+ * VARIABLES
+ **/
+//Constante
+const n1 = 10
+// Variables
+let n2 = 50
+var n3 = 40
+let flag = false
+const suma = n1+n2+n3
+// Mostrar datos por consola
+console.log("Suma: ", suma)
+
+/**Estructura de datos */
+let numeros = [10,20,30,40,50,60]
+console.log(numeros)
+// añadir elementos
+numeros.push(70)
+console.log(numeros)
+// eliminar elementos
+numeros.pop()
+//elimanar un elemento por su posición
+//numeros.pop(2)
+console.log("numeros-> ", numeros)
+// iterar numeros
+for(let i = 0;i<numeros.length; i++){
+  console.log("numeros-> ", numeros[i])
+}
+
+// Objetos
+let persona = {
+  nombre: "Juliana",
+  apellido: "Hernandez",
+  edad: 28,
+  telefonos: ['31234', '4567']
+}
+// Imprimir objeto
+console.log(persona)
+
+//Acceder a los atributos
+console.log(persona.nombre)
+console.log(persona['apellido'])
+// Obtener los valores como un array
+console.log(Object.values(persona))
+// Obtener las llaves como un array
+console.log(Object.keys(persona))
+// Imprimir valores de un objeto
+let values = Object.values(persona)
+for(let i=0; i<Object.values(persona).length; i++){
+  console.log(values[i])
+}
