7e2d7b01261b1ad52ac3163b3daa607ad18624bd
julio sandoval <ingenierojuliosandoval@outlook.com>
2022-09-04 12:34:39 -0500

    introduction-js/rick-and-morty-api: add files index.html, index.css and index.js, too add functions get_characters, show_characters, main and implements api fetch into index.js

---
+++ introduction-js/rick-and-morty-api/index.js
@@ -0,0 +1,34 @@
+async function get_characters(url_api){
+    // Enviar petición Get
+    const resp = await fetch(url_api)
+    // Obtener fatos
+    const data = await resp.json()
+    return data.results
+}
+
+function show_characters(characters){
+    let cards = ''
+    // Iterar arreglo
+    for (let i=0;i<characters.length;i++){
+        cards +=`
+            <article class="card">
+            <img src="${characters[i].image}" />
+            <div class="body-card">
+                <h2>${characters[i].name}</h2>
+                <br/>
+                <span>Specie: ${characters[i].species}</span>
+                <span>Status: ${characters[i].status}</span>
+                <span>Gender: ${characters[i].gender}</span>
+                <span>Origin: ${characters[i].origin.name}</span>
+            </div>
+        </article>`
+    }
+    document.getElementById('section-cards').innerHTML = cards;
+}
+
+async function main(){
+    const url = "https://rickandmortyapi.com/api/character"
+    const characters = await get_characters(url)
+    show_characters(characters)
+}
+main()
