51d4c613c64a0532576114b3fd225ed74bd3ef58
julio sandoval <ingenierojuliosandoval@outlook.com>
2022-09-04 05:03:30 -0500

    introduction-js: add file index.html and index.js

---
+++ introduction-js/index.js
@@ -0,0 +1,13 @@
+// Template String
+let nombre = "Andrés"
+let apellido = "Quintero"
+console.log(`Nombre: ${nombre} Apellido: ${apellido}`)
+// Referenciar div
+const div = document.getElementById('app')
+
+// Adicionar texto
+div.innerText = 'Hola mundo desde java script'
+// Adicionar codigo html
+div.innerHTML = `<h2> Grupo 24</h2>
+  <h4>Introducción a Html con Js</h4>
+  <p>Esto es un ejemplo de un párrafo desde js</p>`
