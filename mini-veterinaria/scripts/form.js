d24e0d56b3306f17f44f506b5a3053cf7439fd74
julio sandoval <ingenierojuliosandoval@outlook.com>
2022-09-16 23:37:30 -0500

    mini-veterinaria: add create into form.js

---
+++ mini-veterinaria/scripts/form.js
@@ -0,0 +1,33 @@
+const URL_API = "http://localhost:8080/mascotas"
+
+function get_data_form(evt){
+    // Indicar por medio del evento que no recargue página
+    evt.preventDefault()
+    const form = evt.target
+    const nombre = form.nombre.value
+    const apellido = form.apellido.value
+    const raza = form.raza.value
+   const mascota = {
+    nombre: form.nombre.value,
+    apellido: form.apellido.value,
+    tipo_mascota: form.tipo_mascota.value,
+    raza: form.raza.value,
+    edad: form.edad.value,
+    observacion: form.observacion.value
+   }
+   create(mascota)
+}
+
+async function create(mascota){
+    // Enviar petición
+    const resp = await fetch(URL_API, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(mascota)
+    })
+
+    const text = await resp.text()
+    alert(text)
+}
