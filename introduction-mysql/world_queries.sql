9d1f2cdf705fdab5b4aa48c25e3a726f70f310c4
julio sandoval <ingenierojuliosandoval@outlook.com>
2022-09-25 17:43:59 -0500

    add Despliegue en Heroku de un Backend.pdf

--- introduction-mysql/world_queries.sql
+++ introduction-mysql/world_queries.sql
@@ -13,3 +13,5 @@ SELECT COUNT(c.Name) num_ciudades, cl.Language FROM country AS c
 	INNER JOIN city ci ON ci.CountryCode = c.Code
 	INNER JOIN countrylanguage cl ON cl.CountryCode = c.Code
 	WHERE c.Code = 'COL' GROUP BY cl.Language;
+    
+SELECT * FROM countrylanguage where CountryCode = "COL";
